25cddfe21a48b53af5b6009e65a29398ceb89854
VihangaIndeewra <indeewravihnga@gmail.com>
2023-07-10 11:57:19 +0530

    Added Loging Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import { Header } from './components/header/Header';
+import { Login } from './components/pages/login/Login';
+
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Login/>
     </div>
   );
 }
---
+++ frontend/src/components/pages/login/Login.jsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
+import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+import Link from '@mui/joy/Link';
+
+
+
+export const Login=()=>{
+    return(
+        <div>
+         <CssVarsProvider>
+             <main>
+                <Sheet
+                sx={{
+                    width: 300,
+                    mx: 'auto', // margin left & right
+                    my: 4, // margin top & bottom
+                    py: 3, // padding top & bottom
+                    px: 2, // padding left & right
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 2,
+                    borderRadius: 'sm',
+                    boxShadow: 'md',
+                }}
+                variant="outlined"
+                >
+                <div>
+                    <Typography level="h4" component="h1">
+                    <b>Welcome!</b>
+                    </Typography>
+                    <Typography level="body2">Log in to continue...</Typography>
+                </div>
+                <FormControl>
+                    <FormLabel>Username</FormLabel>
+                    <Input
+                    // html input attribute
+                    name="username"
+                    type="text"
+                    placeholder="username"
+                    />
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Password</FormLabel>
+                    <Input
+                    // html input attribute
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                    />
+                </FormControl>
+
+                <Button sx={{ mt: 1 /* margin top */ }}>Log in</Button>
+                <Typography
+                    endDecorator={<Link href="/sign-up">Sign up</Link>}
+                    fontSize="sm"
+                    sx={{ alignSelf: 'center' }}
+                >
+                    Don't have an account?
+                </Typography>
+                </Sheet>
+            </main>
+          </CssVarsProvider>
+        </div>
+    );
+}
