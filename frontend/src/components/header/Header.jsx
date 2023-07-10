0f92bc72febdcc51a66e8a1e4c89f35a449e2428
VihangaIndeewra <indeewravihnga@gmail.com>
2023-07-10 09:18:18 +0530

    Added Header

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,33 @@
+export const Header=()=>{
+    return(
+        <div>
+        <div>
+            <nav class="navbar navbar-expand-lg bg-dark bg-body-tertiary">
+            <div class="container-fluid">
+               
+
+                <a class="navbar-brand" >
+                <img src="../assets/images/logo1.jpg" alt="Logo" width="50" height="50" class="d-inline-block align-text-center " style={{"margin-right": "20px"}}/>
+                  Mobile Phone Store
+                </a>
+
+
+
+                {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+                <div class="navbar-nav ">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link disabled">Disabled</a>
+                </div>
+                </div> */}
+            </div>
+            </nav>
+        </div>
+
+    </div>
+    );
+}
