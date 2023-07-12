42550a386b9d28253750da02b9d1c67d7497f6ec
VihangaIndeewra <indeewravihnga@gmail.com>
2023-07-12 10:59:47 +0530

    Added Item Manage Form

--- frontend/src/components/pages/item/Item.jsx
+++ frontend/src/components/pages/item/Item.jsx
@@ -19,6 +19,85 @@ export const Item=()=>{
                      <button className="btnLogout" type="button"  >Log Out</button>
                {/* </Link> */}
             </Header>
+
+            <div>
+            <form id="itemInputForm">
+                <div class="mb-3">
+                    <label for="mobileIdInput" class="form-label">Mobile ID</label>
+                    <input type="text" class="form-control" id="mobileIdInput"/>
+                </div>
+                <div class="mb-3">
+                    <label for="mobileNameInput" class="form-label">Mobile Name</label>
+                    <input type="text" class="form-control" id="mobileNameInput"/>
+                </div>
+                <div class="mb-3 ">
+                <label for="mobileColorInput" class="form-label">Mobile Color</label>
+                    <input type="text" class="form-control" id="mobileColorInput"/>
+                </div>
+                <div class="mb-3 ">
+                <label for="mobileQtyInput" class="form-label">Qty</label>
+                    <input type="text" class="form-control" id="mobileQtyInput"/>
+                </div>
+                <div class="mb-3 ">
+                <label for="mobileUnitPriceInput" class="form-label">Unit Price</label>
+                    <input type="text" class="form-control" id="mobileUnitPriceInput"/>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+            
+{/* 
+            <form id="itemInputForm">
+            <br/>
+            <div className="divFirst">
+                <div className="divFirstChild">
+                    <label className="name" htmlFor='mobileId'>Mobile Id</label>
+                    <br />
+                    <input type="text" id='mobileId' name='mobileId' value={mobileId} />
+                </div>
+
+                <div className="divFirstChild">
+                    <label className="name" htmlFor='bookTitle'>Mobile Name</label>
+                    <br />
+                    <input type="text" id='bookTitle' name='bookTitle' value={bookTitle} onChange={(e)=>{setBookTitle(e.target.value)}} />
+                </div>
+
+                <div className="divFirstChild">
+                    <label className="name" htmlFor='bookAuthor'>Author</label>
+                    <br />
+                    <input type="text" id='bookAuthor'name='bookAuthor' value={bookAuthor} onChange={(e)=>{setBookAuthor(e.target.value)}}/>
+                </div>
+
+           </div>
+           <br/>
+
+           <div className="divFirst">
+                <div className="divSecondChild">
+                   <label className="name" htmlFor='bookLanguage'>Language</label>
+                    <br />
+                    <input type="text" id='bookLanguage' name='bookLanguage' value={bookLanguage} onChange={(e)=>{setBookLanguage(e.target.value)}}/>
+                </div>
+
+                <div className="divSecondChild">
+                    <label className="name" htmlFor='bookPrice'>Price</label>
+                    <br />
+                    <input type="text" id='bookPrice' name='bookPrice' value={bookPrice} onChange={(e)=>{setBookPrice(e.target.value)}}/>
+                </div>
+            </div>    
+
+            <br/>
+            <br/>
+
+            <div id="btnGroup">
+                <button id="btnSave" type="button" onClick={saveBook}>Save</button>
+           
+                <button id="btnUpdate" type="button" onClick={updateBook}>Update</button>
+            
+                <button id="btnDelete" type="button" onClick={deleteBook}>Delete</button>
+            </div>
+           </form> */}
+           <br/>
+
         </div>
     );
 }
